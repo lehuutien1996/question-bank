0d6b84e351fe9ab2b0e3a6274b29e055143b70ef
Tien, Le Huu <lehuutien1996@gmail.com>
2016-11-18 20:11:16 +0700

    Added Label for Main Form

--- QuestionBank/QuestionBank/QuestionBank.Designer.cs
+++ QuestionBank/QuestionBank/QuestionBank.Designer.cs
@@ -28,20 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Light", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(322, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Welcome to Question Bank";
             // 
             // QuestionBank
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(608, 518);
+            this.ClientSize = new System.Drawing.Size(520, 449);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "QuestionBank";
             this.Text = "Question Bank";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
